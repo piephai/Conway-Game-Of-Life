c819d288d53696a6bb421f2211d5d40642312c8a
piephai <tonphai2522@gmail.com>
2020-09-14 23:04:16 +1000

    Adding input file and cells can now be displayed in a random fashion

--- Life/Life/Program.cs
+++ Life/Life/Program.cs
@@ -18,10 +18,12 @@ namespace Life
         private static int generations = 50;
         private static string inputFile = "N/A";
         private static float maxUpdateRate = 5f;
+        private static char[][] cells = new char[rows][];
+
 
         static void Main(string[] args)
         {
-        //GridDimensions gridDimensions = new GridDimensions();
+            //GridDimensions gridDimensions = new GridDimensions();
             string[] arrayAll = new string[args.Length];
             var listOfInts = new List<int>();
             var indexOption = new List<int>();
@@ -51,7 +53,7 @@ namespace Life
 
                     }
                 }
-                
+
 
                 int numParaSinceOption = 0;
                 indexOption.Add(0);
@@ -74,7 +76,7 @@ namespace Life
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     correctInput = OptionArgument(arrayAll, indexOption.ToArray()[i], listOfInts.ToArray()[i]);
-                    
+
                 }
                 if (!correctInput)
                 {
@@ -103,24 +105,27 @@ namespace Life
             // Feel free to remove or modify it when you are comfortable with it.
 
             // Specify grid dimensions and active cells...
-            
-            
-            int[,] cells = {
-                { 5, 3 },
-                { 4, 3 },
-                { 5, 5 },
-                { 4, 5 },
-                { 2, 1 },
-                { 1, 2 },
-                { 1, 3 },
-                { 1, 4 },
-                { 1, 5 },
-                { 1, 6 },
-                { 2, 7 }
-            };
+
+
+            //int[,] cells = {
+            //    { 5, 3 },
+            //    { 4, 3 },
+            //    { 5, 5 },
+            //    { 4, 5 },
+            //    { 2, 1 },
+            //    { 1, 2 },
+            //    { 1, 3 },
+            //    { 1, 4 },
+            //    { 1, 5 },
+            //    { 1, 6 },
+            //    { 2, 7 }
+            //};
+
+
 
             // Construct grid...
             Grid grid = new Grid(rows, columns);
+
 
             // Wait for user to press a key...
             Console.WriteLine("Press spaacebar to start...");
@@ -133,40 +138,52 @@ namespace Life
             grid.SetFootnote(0.ToString());
 
             Stopwatch watch = new Stopwatch();
+            Array enumValues = Enum.GetValues(typeof(CellState));
+            Random random = new Random();
+
+
+            //for (int i = 0; i < totalCells; i++)
+            //{
+            //    cells[i, 0] = currentRow;
+            //    cells[i, 1] = currentColumn;
+
+            //    currentColumn++;
+            //    currentRow++;
+            //}
+            //int totalCells = rows * columns;
+            //InitialiseCellBuffer(ref cells, rows, columns);
 
             // For each of the cells...
-            for (int i = 0; i < cells.GetLength(0); i++)
+            for (int i = 0; i < generations; i++)
             {
-                watch.Restart();
+                for (int row = 0; row < rows; row++)
+                {
+                    for (int column = 0; column < columns; column++)
+                    {
+                        watch.Restart();
 
-              
-               
+                        CellState cellState = (CellState)enumValues.GetValue(random.Next(0, 2));
 
-                // Update grid with a new cell...
-                grid.UpdateCell(cells[i, 0], cells[i, 1], CellState.Full);
+                        // Update grid with a new cell...
+                        grid.UpdateCell(row, column, cellState);
 
-                // Render updates to the console window...
-                grid.Render();
-                
-                
-                // Update grid with a new cell...
-                grid.UpdateCell(cells[i, 0], cells[i, 1], CellState.Full);
+                        // Render updates to the console window...
+                        grid.Render();
 
-                // Render updates to the console window...
-                grid.Render();
+                    }
+                }
                 if (stepMode)
                 {
                     while (Console.ReadKey().Key != ConsoleKey.Spacebar) ;
-                    grid.SetFootnote((i+1).ToString());
+
                 }
                 else
                 {
                     while (watch.ElapsedMilliseconds < 1000 / maxUpdateRate) ;
                 }
-                
-
+                grid.SetFootnote((i + 1).ToString());
             }
-            grid.SetFootnote(cells.GetLength(0).ToString());
+            grid.SetFootnote(generations.ToString());
 
 
             // Set complete marker as true
@@ -185,7 +202,7 @@ namespace Life
         //Determine which optional argument the user has inputted in and verify whether the user input the correct parameter for the option  
         public static bool OptionArgument(string[] arr, int startingIndex, int numOfParam)
         {
-            
+
 
             if (arr[startingIndex] == "--dimensions")
             {
@@ -204,7 +221,7 @@ namespace Life
 
                         Console.WriteLine("Dimension is out of range. " +
                             "Rows and columns must be a positive integer between 4 - 48 (inclusive) ");
-                        
+
                         //errorList.Add(1);
                         return false;
                     }
@@ -213,7 +230,7 @@ namespace Life
                 {
                     Console.WriteLine("Dimensions has two parameters:" +
                         " --dimensions <rows> <columns> which is a positive integer between 4 - 48 (inclusive)");
-                    
+
                     //errorList.Add(1);
                     return false;
                 }
@@ -233,11 +250,11 @@ namespace Life
                 else
                 {
                     Console.WriteLine("--step takes no parameters but was given one");
-                    
+
                     return false;
                 }
             }
-            
+
             else if (arr[startingIndex] == "--random")
             {
                 if (numOfParam == 1)
@@ -251,7 +268,7 @@ namespace Life
                     else
                     {
                         Console.WriteLine("Random factor must be a float between 0 and 1 (inclusive)");
-                        
+
                         return false;
                     }
                 }
@@ -263,9 +280,9 @@ namespace Life
                 }
                 else
                 {
-                    Console.WriteLine("Random has one parameter but more than one parameter was provided" + 
+                    Console.WriteLine("Random has one parameter but more than one parameter was provided" +
                         "--random <probability> where probability is a float between 0 and 1 (inclusive)");
-                    
+
                     return false;
                 }
             }
@@ -274,7 +291,7 @@ namespace Life
                 if (numOfParam == 1)
                 {
                     int tempGeneration = 0;
-                    if (int.TryParse(arr[startingIndex], out tempGeneration))
+                    if (int.TryParse(arr[startingIndex + 1], out tempGeneration))
                     {
                         generations = tempGeneration;
                         return true;
@@ -282,18 +299,18 @@ namespace Life
                     else
                     {
                         Console.WriteLine("Generation value must be a positive non-zero integer");
-                       
-                        return false; 
+
+                        return false;
                     }
                 }
                 else
                 {
                     Console.WriteLine("--generations <number> only take one parameter");
-                    
+
                     return false;
                 }
             }
-       
+
             else if (arr[startingIndex] == "--seed")
             {
                 if (numOfParam == 1)
@@ -306,18 +323,18 @@ namespace Life
                     else
                     {
                         Console.WriteLine("The path provided is not in the right .seed format");
-                        
+
                         return false;
                     }
                 }
                 else
                 {
                     Console.WriteLine("--seed <filename> only take one parameter");
-                    
+
                     return false;
                 }
             }
-       
+
             else if (arr[startingIndex] == "--max-update")
             {
                 if (numOfParam == 1)
@@ -331,14 +348,14 @@ namespace Life
                     else
                     {
                         Console.WriteLine("Max update rate must be a float between 1 and 30 (inclusive)");
-                        
+
                         return false;
                     }
                 }
                 else
                 {
                     Console.WriteLine("--max-update <update per second> Only take one parameter");
-                    
+
                     return false;
                 }
 
@@ -348,44 +365,33 @@ namespace Life
                 return false;
             }
 
-
-            //string result = string.Empty;
-            //if (amountToPrint >= 2)
-            //{
-            //    result += arr[startingIndex] + ": ";
-            //    result += "{ ";
-            //    result += arr[startingIndex + 1];
-            //    for (int i = startingIndex + 2; i <= (startingIndex + amountToPrint); i++)
-            //    {
-            //        result += ", ";
-            //        result += arr[i];
-
-            //    }
-            //    result += " }";
-            //}
-            //else if (amountToPrint > 0 && amountToPrint <= 1)
-            //{
-            //    result += arr[startingIndex] + ": ";
-            //    result += "{ ";
-            //    result += arr[startingIndex + 1];
-            //    result += " }";
-            //}
-            //else
-            //{
-            //    result += arr[startingIndex];
-            //}
-            //return result;
         }
 
+
+        //public static char [][] InitialiseCellBuffer (ref char [][] cells, int rows, int columns)
+        //{
+        //    Cell cell = new Cell();
+        //    int currentRow = 0;
+        //    int currentColumn = 0;
+
+        //    for(int i = 0; i < rows; i++)
+        //    {
+        //        cells[i] = new char[columns];
+        //    }
+
+        //    cell.Draw(ref cells, currentRow, currentColumn, columns, rows);
+
+        //    return cells;
+        //}
         public static void PrintProgramSetting(int rows, int columns, bool periodic, bool stepMode, float randomFactor, int generations, string inputFile, float maxUpdateRate)
         {
 
             Console.WriteLine(String.Format("\nThe program will use the following settings:\n"));
             int wordPosition = inputFile.Length + 10;
 
-            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Input File: ", inputFile) );
+            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Input File: ", inputFile));
             Console.WriteLine(String.Format("{0, 27}{1, -26}", "Generations: ", generations));
-            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Refresh rate: ", maxUpdateRate )); /// ADD
+            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Refresh rate: ", maxUpdateRate)); /// ADD
             if (periodic)
             {
                 Console.WriteLine(String.Format("{0, 27}{1, -26}", "Periodic: ", "Yes"));
@@ -394,7 +400,7 @@ namespace Life
             {
                 Console.WriteLine(String.Format("{0, 27}{1, -26}", "Periodic: ", "No"));
             }
-            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Rows: " ,rows));
+            Console.WriteLine(String.Format("{0, 27}{1, -26}", "Rows: ", rows));
             Console.WriteLine(String.Format("{0, 27}{1, -26}", "Columns: ", columns));
             Console.WriteLine(String.Format("{0, 27}{1, -26}", "Random Factor: ", randomFactor + "%"));
             if (stepMode)
@@ -407,49 +413,53 @@ namespace Life
             }
         }
 
-        //public static void CheckeERrorMesssage()
-        //{
-            
+        public static void ReadInputFile()
+        {
 
-        //    foreach (int item in caseList)
-        //        {
-        //        switch (caseList[item])
-        //    {
-        //            case 1:
-        //                Console.WriteLine("wrong");
-        //                break;
+            using (StreamReader file = new StreamReader(inputFile))
+            {
+                List<int> rowList = new List<int>();
+                List<int> columnList = new List<int>();
 
-        //                //case 2:
-        //                //    Console.WriteLine();
-        //                //    break;
+                string line;
+                string[] fields;
+                const char DELIM = ' ';
+                int row;
+                int column;
+                file.ReadLine();
+                while ((line = file.ReadLine()) != null)
+                {
+                    fields = line.Split(DELIM);
 
-        //                //case 3:
-        //                //    Console.WriteLine();
-        //                //    break;
+                    row = Convert.ToInt32(fields[0]);
 
-        //                //case 4:
-        //                //    Console.WriteLine();
-        //                //    break;
+                    rowList.Add(row);
 
-        //                //case 5:
-        //                //    Console.WriteLine();
-        //                //    break;
+                    column = Convert.ToInt32(fields[1]);
 
-        //                //case 6:
-        //                //    Console.WriteLine();
-        //                //    break;
+                    columnList.Add(column);
+                }
 
-        //                //case 7:
-        //                //    Console.WriteLine();
-        //                //    break;
+                int[][] location = new int[rowList.Count][];
 
-        //                //case 8:
-        //                //    Console.WriteLine();
-        //                //    break;
-        //        }
-        //    }
-        //}
+                for (int i = 0; i < rowList.Count; i++)
+                {
+                    location[i] = new int[2];
+                }
 
-        
+                for (int i = 0; i < location.Length; i++)
+                {
+                    location[i][0] = rowList[i];
+                    location[i][1] = columnList[i];
+                }
+
+                for (int i = 0; i < location.Length; i++)
+                {
+                    Console.WriteLine("Row: {0}\nColumn: {1}", location[i][0], location[i][1]);
+                }
+            }
+
+
+        }
     }
 }
